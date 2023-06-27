2732aa2a501247b78e41cbc8123880273eababec
Nikita <tulenckov.nikita@gmail.com>
2023-06-28 00:15:32 +0300

    add fulfillment of the last 2 points

--- tests_password.py
+++ tests_password.py
@@ -22,11 +22,31 @@ def check_lowercase_letters(string):
     return False
 
 
-def test_password(password, email=None):
+def check_special_characters(string):
+    """Проверка наличия спец символов"""
+    special_characters = ["!", "@", "#", "$"]
+    for char in special_characters:
+        if char in string:
+            return True
+    return False
+
+
+def check_password(username, password):
+    """Проверка логина и пароля на схожесть"""
+    if username.lower() in password.lower():
+        return False
+    return True
+
+
+def test_password(password, email):
     assert len(password) >= 8, print('Пароль должен содержать не менее 8 символов')
     assert check_uppercase_letters(password) is True, print('Пароль должен содержать хотя бы одну заглавную букву')
     assert check_lowercase_letters(password) is True, print('Пароль должен содержать хотя бы одну строчную букву')
     assert check_digits(password) is True, print('Пароль должен содержать хотя бы одну цифру')
+    assert check_special_characters(password) is True, print('Пароль должен содержать хотя '
+                                                             'бы один специальный символ из перечисленных: !, @, #, $')
+    assert check_password(email, password) is True, print('Пароль не должен содержать подстроку,'
+                                                          ' совпадающую с частью имени ')
 
 
-test_password('33333333333333')
+test_password('пароль', 'логин')
